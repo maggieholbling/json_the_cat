8ca854a9be0bdb2fdb8fa55ed8f0604ae5de751b
Maggie <maggieholbl@gmail.com>
2019-08-29 20:53:10 +0000

    added error handlers

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,12 @@
 const request = require('request');
 
-const getBreeds = (string) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${string}`, (error, response, body) => {
-    if (error) return console.log("there was an error");
+const getBreeds = (breed) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) return console.log(`${error}`);
     if (response.statusCode !== 200) return console.log(`Error: ${response.statusCode}`);
-
+    
     const data = JSON.parse(body);
+    if (!data[0]) return console.log(`Error: ${breed} breed is not found`);
     console.log(data[0]["description"]);
   });
 }
